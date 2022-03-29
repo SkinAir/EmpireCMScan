6e8e2a90f0afcdb8360ef2f88e8ef43bdc69d398
SkinAir <71880910+SkinAir@users.noreply.github.com>
2022-03-29 15:51:17 +0800

    EmpireDir

---
+++ EmpireDir/e/data/adminlogin/user1_feabf878327f505a84db3ceb6fa6d6e2.php
@@ -0,0 +1,12 @@
+<?php
+$ecms_adminloginr=array();
+$ecms_adminloginr=Array('userid'=>'1',
+'ernd'=>'7BwwitFYLbqlVz8Gk5TmFJftgDH',
+'erndtwo'=>'iDB2eZCDItcbTOtZCyBC',
+'erndadd'=>'Nw2uX8B96TPVORVVvjD48zMGQ5vUvhuh',
+'ehash'=>'iiBkqYSGdQqGj9kEmHak',
+'ehashname'=>'ehash_5msR',
+'rhash'=>'06dJ6UdxIf7t',
+'rhashname'=>'rhash_XAKZ',
+'edbdata'=>'1');
+?>
