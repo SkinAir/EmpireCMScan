7b9e3682613597451e2c29b7366d6f2428e47eb2
SkinAir <71880910+SkinAir@users.noreply.github.com>
2022-04-02 00:51:03 +0800

    file

--- CMScan.py
+++ CMScan.py
@@ -2,9 +2,14 @@ from ast import arg
 import threading
 import requests
 from queue import Queue
-import random
 from colorama import Fore, Back, Style
 import argparse
+import os
+import sys
+import threading, time, signal
+#用以ctrl+c
+
+os.chdir('D:\Air-Rolan\Air\漏洞扫描\Empirecmscan')
 
 
 #目录遍历
@@ -22,33 +27,58 @@ class Dirscan(threading.Thread):
                 }
                 #发送请求
                 res = requests.get(url=url,headers=headers)
+                lock.acquire()#获取锁
                 if res.status_code == 200:
                     print(Fore.GREEN + "[+] Success is %s : 200 " % url)
+                    with open('result.txt','a',encoding='utf-8')as f:
+                        f.write(Fore.GREEN + "[+] Success is %s : 200 " % url + '\n')
+                        f.close()
                 elif res.status_code !=200 :
                     print(Fore.RED + "[-] Fail is %s : %d" % (url,res.status_code))
+                    with open('result.txt','a',encoding='utf-8')as f:
+                        f.write(Fore.RED + "[-] Fail is %s : %d" % (url,res.status_code) + '\n')
+                        f.close()
+                lock.release()
             except Exception as e:
                 print("主机拒绝连接")
 
+lock = threading.Lock()
+
 #启动函数
 def start(url,web,count):
-    queue = Queue()
-#引用目录文件
-    with open("%s.txt" % web, "r")as f:
-        for i in f:
-            newUrl = url + i.strip('\n')
-            queue.put(newUrl)
+    try:
+        signal.signal(signal.SIGINT, quit)
+        signal.signal(signal.SIGTERM, quit)
 
-    threads = []
-    thread_conut = int(count)
-#多线程
-    for i in range(thread_conut):
-        threads.append(Dirscan(queue))
-    
-    for t in threads:
-        t.start()
+        queue = Queue()
+    #引用目录文件
+        with open("%s.txt" % web, "r")as f:
+            for i in f:
+                newUrl = url + i.strip('\n')
+                queue.put(newUrl)
 
-    for t in threads:
-        t.join()
+        threads = []
+        thread_conut = int(count)
+    #多线程
+        for i in range(thread_conut):
+            threads.append(Dirscan(queue))
+        
+        for t in threads:
+            t.setDaemon(True)
+            t.start()
+
+        for t in threads:
+            t.join()
+    except Exception as e:
+        print(e)
+
+def clear():
+    file = open("result.txt", 'w').close()#清空文件内容
+
+#定义ctrl+c
+def quit(signum, frame):
+    print('You choose to stop me.')
+    sys.exit()
 
 #定义命令行参数
 def parse_args():
@@ -61,6 +91,7 @@ def parse_args():
     return args
 
 if __name__ == '__main__':
+    clear()
     args = parse_args()
     url = str(args.url)
     web = "DIR"
